bd888a7f1f70ac3ae71eebb8aaa656fb61b91808
Hikikomori <prusakov.ms@gmail.com>
2017-07-31 17:32:45 +0500

    Добавление формы обратной связи

--- protechs/js/script.js
+++ protechs/js/script.js
@@ -1,9 +1,35 @@
-$(document).ready(function(){
-	$.easing.def = "easeInOutQuad";
-	$('li.button a').click(function(e){
-		var dropDown = $(this).parent().next();
-		$('.dropdown').not(dropDown).slideUp('slow');
-		dropDown.slideToggle('slow');
-		e.preventDefault();
-	})	
-});
+var popup=document.querySelector(".popup");
+var popupClose=document.querySelector(".popup__close");
+var popupCheck=document.querySelector(".popup__input");
+
+var feedback=document.querySelector(".feedback");
+var feedbackOpen=document.querySelector(".open-feedback");
+var feedbackClose=document.querySelector(".feedback__close");
+
+if (popup) {
+    $("#popup__text").load("action.txt",function() {
+        if ($("#popup__text").html() && !localStorage.getItem("popupNoShow")) {
+            popup.classList.add("popup--visible");
+        }
+    });
+
+    popupClose.addEventListener("click", function(event){
+        event.preventDefault();
+        if(popupCheck.checked){
+            localStorage.setItem("popupNoShow",true);
+        }
+        popup.classList.remove("popup--visible");
+    });
+}
+
+if (feedback) {
+    feedbackOpen.addEventListener("click", function(event){
+        event.preventDefault();
+        feedback.classList.add("feedback--visible");
+    });
+
+    feedbackClose.addEventListener("click", function(event){
+        event.preventDefault();
+        feedback.classList.remove("feedback--visible");
+    });
+}
